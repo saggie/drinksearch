c5f5ccea803cf841ec0693c4c342490f3d9c8383
Tomoyuki Sagiyama <tomoyuki.sagiyama.ff@hitachi.com>
2017-12-26 14:39:21 +0900

    Implemented 'GET drinks'

--- hello/views.py
+++ hello/views.py
@@ -26,7 +26,22 @@ def teapot(request):
     return HttpResponse('<pre>' + r.text + '</pre>')
 
 def drinks(request):
-    data = {'id': 1, 'name': 'hoge'}
+    drinks = []
+    with open('drinks.csv', 'r') as f:
+        reader = csv.reader(f)
+        header = next(reader)  # skip header
+        for row in reader:
+            drink = {
+                'name': row[0],
+                'price': row[1],
+                'is_cold': row[2],
+                'is_emoney': row[3],
+                'floor': row[4],
+                'location': row[5],
+                'category': row[6]
+            }
+            drinks.append(drink)
+    data = { 'data': drinks }
     json_str = json.dumps(data, ensure_ascii=False, indent=2)
     response = HttpResponse(json_str, content_type='application/json; charset=UTF-8')
     return response
