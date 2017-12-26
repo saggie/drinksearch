4e26841f5b7d3a8da8dc5d8b6e91cc575b69c76a
Tomoyuki Sagiyama <tomoyuki.sagiyama.ff@hitachi.com>
2017-12-26 17:43:04 +0900

    Implemented category filter

--- hello/views.py
+++ hello/views.py
@@ -30,17 +30,25 @@ def drinks(request):
     with open('drinks.csv', 'r') as f:
         reader = csv.reader(f)
         header = next(reader)  # skip header
+
         for row in reader:
             drink = [
-                row[0],
-                row[1],
-                row[2],
-                row[3],
-                row[4],
-                row[5],
-                row[6]
+                row[0], # name
+                row[1], # price
+                row[2], # cold
+                row[3], # emoney
+                row[4], # floor
+                row[5], # location
+                row[6]  # category
             ]
+
+            # filter by category
+            param_category = request.GET.get('category', '')
+            if param_category != '' and param_category != drink[6]:
+                continue
+
             drinks.append(drink)
+
     data = { 'data': drinks }
     json_str = json.dumps(data, ensure_ascii=False, indent=2)
     response = HttpResponse(json_str, content_type='application/json; charset=UTF-8')
